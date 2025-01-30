d8961b274412decfda27c6f89f784b69583cb6e4
Anuran12 <anuranarts@gmail.com>
2025-01-31 02:40:22 +0530

    fix

--- src/app/api-doc/page.tsx
+++ src/app/api-doc/page.tsx
@@ -2,7 +2,7 @@ import { getApiDocs } from "../../../lib/swagger";
 import ReactSwagger from "./react-swagger";
 
 export default async function IndexPage() {
-  const spec = await getApiDocs();
+  const spec = (await getApiDocs()) as Record<string, unknown>;
   return (
     <section>
       <ReactSwagger spec={spec} />
