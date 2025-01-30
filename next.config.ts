04fcd1fa94e9745e9240fcb9e573717f1f9ff670
Anuran12 <anuranarts@gmail.com>
2025-01-31 04:25:45 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone",
-  swcMinify: true,
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
